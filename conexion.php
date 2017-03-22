ba7fd71b70ee860b9c658da588a755c2b4fc4aba
Vespertino <Vespertino@C6PC15>
2017-03-22 17:52:26 +0100

    Conexion con PDO

---
+++ conexion.php
@@ -0,0 +1,38 @@
+<?php
+
+//
+//$serverName = "C6PC15\SQLEXPRESS";
+//$conInfo = array("Database" => "CadenaTiendas");
+//
+//$conn = sqlsrv_connect($serverName, $conInfo);
+//
+//if ($conn) {
+//    echo "Conexión establecida.<br />";
+//} else {
+//    echo "Conexión no se pudo establecer.<br />";
+//    die(print_r(sqlsrv_errors(), true));
+//}
+//
+//Conexion con PDO
+
+$c = new PDO("sqlsrv:Server=localhost;Database=CadenaTiendas", "", "");
+
+
+
+//Para probar que funciona la conexion:
+
+//try{
+//$c = new PDO("sqlsrv:Server=localhost;Database=CadenaTiendas", "", "");
+//
+// foreach($c->query('SELECT * from articulo') as $fila) {
+//        print_r($fila);
+//    }
+//    $c = null;
+//} catch (PDOException $e) {
+//    print "¡Error!: " . $e->getMessage() . "<br/>";
+//    die();
+//}
+
+//Para cerrar la conexion
+
+//$c = null;
