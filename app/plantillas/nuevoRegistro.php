ac68054fd0d457bf17bb04181ee19883bb912487
Vespertino <Vespertino@C6PC15>
2017-05-17 17:55:26 +0200

    .

--- app/plantillas/nuevoRegistro.php
+++ app/plantillas/nuevoRegistro.php
@@ -8,11 +8,18 @@
     </head>
     <body id="registro">
         <div >
+            
+            <!--Añadir que se puede ver/modificar el perfil del usuario-->
             <h1>Formulario de registro para nuevos usuarios</h1>
-
             <form action="index.php?ctl=nuevoRegistro" method="POST">
                 Usuario: <input type="text" name="usuario" value=""required> <br>
                 Contraseña: <input type="password" name="contra" required><br>
+                <b>Datos de facturación</b><br>
+                Nombre: <input type="text" name="nombre" ><br>
+                Apellidos: <input type="text" name="apellidos" ><br>
+                Direccion: <input type="text" name="direccion" ><br>
+                Ciudad: <input type="text" name="ciudad" ><br>
+                C.P: <input type="text" name="cp" ><br>
                 <input type="submit" value="Registrarse">
             </form>
             <a href="index.php?ctl=inicio">Inicio</a>
